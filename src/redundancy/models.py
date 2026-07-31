970dd7f6f6e7ebe29c0ac9c64bae97cef93119f1
Emir <emir05051@gmail.com>
2026-07-31 14:50:54 +0000

    add tokens to the model downloading

--- src/redundancy/models.py
+++ src/redundancy/models.py
@@ -1,5 +1,10 @@
+import os
+
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from accelerate import Accelerator
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class RedundancyModel:
@@ -8,7 +13,12 @@ class RedundancyModel:
         self.device = Accelerator().device
 
         local_model_cache = "./configs/models"
+        hf_token = os.getenv("HF_TOKEN")
 
-        self.tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir=local_model_cache)
-        self.model = AutoModelForCausalLM.from_pretrained(model_name, cache_dir=local_model_cache)
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            model_name, cache_dir=local_model_cache, token=hf_token
+        )
+        self.model = AutoModelForCausalLM.from_pretrained(
+            model_name, cache_dir=local_model_cache, token=hf_token
+        )
         self.model.to(self.device)
